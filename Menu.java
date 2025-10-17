0226a6d07f381d05bd9a33317288b0489109b4fb
Bob283527 <jonjiang27@7hills.org>
2025-10-17 09:25:46 -0400

    fixed some simple mistakes accidently made Menu a String method instead of a constructor

--- Menu.java
+++ Menu.java
@@ -1,16 +1,23 @@
-java.util.Scanner;
+import java.util.Scanner;
 
 public class Menu {
 	
 	Scanner scanner = new Scanner(System.in);
 	
-	public String Menu() {
+	public Menu() {
 		System.out.println("Type 1 for AI mode");
 		System.out.println("Type 2 for PvP mode");
 		System.out.println("Type r for Rules");
-		Scanner choice = scanner.nextLine();
+		String choice = scanner.nextLine();
 		if (choice.equals("r")) {
-			
+			//i want to call the rules from readme idk how
+			System.out.println("test");
 		}
+		
+	}
+	public static void main(String[] args) {
+	Menu test = new Menu();
+}
+
 }
 
