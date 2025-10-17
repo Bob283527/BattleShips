17ddb2f6b6256d5db3ba11cbdd21ca9c3711b0b8
Bob283527 <jonjiang27@7hills.org>
2025-10-17 08:58:10 -0400

    Working on menu

---
+++ Menu.java
@@ -0,0 +1,16 @@
+java.util.Scanner;
+
+public class Menu {
+	
+	Scanner scanner = new Scanner(System.in);
+	
+	public String Menu() {
+		System.out.println("Type 1 for AI mode");
+		System.out.println("Type 2 for PvP mode");
+		System.out.println("Type r for Rules");
+		Scanner choice = scanner.nextLine();
+		if (choice.equals("r")) {
+			
+		}
+}
+
